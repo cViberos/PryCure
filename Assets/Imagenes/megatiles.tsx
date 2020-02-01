4852d5062dba6d05d923994dbd08f7383aa3cd00
Gonzalo Ezequiel Bordon <bordongonzaloezequiel@gmail.com>
2020-02-01 12:51:40 -0300

    mapitas

---
+++ Assets/Imagenes/megatiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="megatiles" tilewidth="64" tileheight="64" tilecount="28" columns="4">
+ <image source="tileset.png" width="256" height="448"/>
+</tileset>
