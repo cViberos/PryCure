616adee7ce353a6db405f9bba5e509a6575b16f5
unknown <Viberos993@outlook.com>
2020-02-01 17:47:18 -0300

    4 commit

--- Assets/mousePointer.cs
+++ Assets/mousePointer.cs
@@ -8,6 +8,8 @@ public class mousePointer : MonoBehaviour
     private RaycastHit hit;
     public Camera camera;
     private Vector3 posCamera;
+    public float posix;
+    public float posiy;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +27,29 @@ public class mousePointer : MonoBehaviour
             Vector3 pos = new Vector3(hit.point.x, hit.point.y, 0);
             transform.position = pos;
             //selectedTile.transform.position = new Vector3(pos.x, pos.y, 0.1f);
-            selectedTile.transform.position = new Vector3( (int)pos.x, (int)pos.y, 0.1f );
+            
+            posix=pos.x-(int)pos.x;
+            posiy=System.Math.Abs (pos.y-(int)pos.y);
+
+            if (posix >= 0.5f)
+            {
+                selectedTile.transform.position = new Vector3( (int)pos.x+2, (int)pos.y, 0.1f );
+            }else
+            {
+                //selectedTile.transform.position = new Vector3( (int)pos.x+1, (int)pos.y, 0.1f );
+            }
+
+            if (posiy <= 0.5f)
+            {
+                selectedTile.transform.position = new Vector3( (int)pos.x, (int)pos.y, 0.1f );
+            }else
+            {
+                selectedTile.transform.position = new Vector3( (int)pos.x, (int)pos.y-1, 0.1f );
+            }
+
+
+
+            //selectedTile.transform.position = new Vector3( (int)pos.x, (int)pos.y, 0.1f );
             //camera.transform.position = posCamera + transform.position;
             //posCamera = camera.transform.position + new Vector3(8, 8, -3);
             //camera.transform.position = posCamera;
