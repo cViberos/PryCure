0ad6750880ae5a37e14ec3eaaa3c3ce62053bbdc
unknown <krytor45@gmail.com>
2020-02-02 02:40:11 -0300

    Nodos

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -17,12 +17,22 @@ public class gameManager : MonoBehaviour
     public GameObject[] tilesJugador1;
     private int cantTilesJ1 = 0;
     public GameObject[] tilesJugador2;
-    private int cantTilesJ2 = 0;    
+    private int cantTilesJ2 = 0;
     public GameObject cartelJuegaHumano;
     public GameObject cartelJuegaVirus;
 
+
+    //Prefabs Virus y Hospitales
+    public GameObject[] prefabVirus;
+    public GameObject[] prefabSalud;
+
+    public int recursosHumanos;
+    public int recursosVirus;
+
     void Start()
     {
+        recursosHumanos = 0;
+        recursosVirus = 0;
         sizeX = generador.GetComponent<tileGenerator>().sizeX;
         sizeY = generador.GetComponent<tileGenerator>().sizeY;
 
@@ -39,7 +49,7 @@ public class gameManager : MonoBehaviour
             {
 
 
-                matrixDisp[i,j] = Instantiate(disponibleTile, new Vector3(j, -i, -0.2f), Quaternion.identity);
+                matrixDisp[i, j] = Instantiate(disponibleTile, new Vector3(j, -i, -0.2f), Quaternion.identity);
                 matrixDisp[i, j].SetActive(true);
                 matrixDisp[i, j].GetComponent<tileData>().posX = j;
                 matrixDisp[i, j].GetComponent<tileData>().posY = i;
@@ -59,91 +69,126 @@ public class gameManager : MonoBehaviour
     void Update()
     {
 
+
+
         print("Es el turno:" + turn);
-
-        if (turn % 2 == 0)
-        {
-            cartelJuegaHumano.SetActive(true);
-            cartelJuegaVirus.SetActive(false);
-
-        }
-        else
-        {
-            cartelJuegaHumano.SetActive(false);
-            cartelJuegaVirus.SetActive(true);
-        }
 
         LayerMask layerMask = ~(1 << 9);
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit, 100f, layerMask))
+        if (turn <= 16)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Physics.Raycast(ray, out hit, 100f, layerMask))
             {
-
-                if (hit.transform.name == "TILE(Clone)")
+                if (Input.GetMouseButtonDown(0))
                 {
-                    int posX = hit.transform.gameObject.GetComponent<tileData>().posX;
-                    int posY = hit.transform.gameObject.GetComponent<tileData>().posY;
 
-                    sizeX = generador.GetComponent<tileGenerator>().sizeX;
-                    sizeY = generador.GetComponent<tileGenerator>().sizeY;
+                    if (hit.transform.name == "TILE(Clone)")
+                    {
+                        int posX = hit.transform.gameObject.GetComponent<tileData>().posX;
+                        int posY = hit.transform.gameObject.GetComponent<tileData>().posY;
 
-                    int k = 0;
-                    for (int i = 0; i < sizeY; i++)
-                    {                  
-                        for (int j = 0; j < sizeX; j++)
+                        sizeX = generador.GetComponent<tileGenerator>().sizeX;
+                        sizeY = generador.GetComponent<tileGenerator>().sizeY;
+
+                        int k = 0;
+                        for (int i = 0; i < sizeY; i++)
                         {
-                            if (i == posY && j == posX)
+                            for (int j = 0; j < sizeX; j++)
                             {
-                                NoDisponibleArray[k] = true;
-                                matrixDisp[i, j].GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);
-                                if (turn % 2 == 0)
+                                if (i == posY && j == posX)
                                 {
+                                    /* NoDisponibleArray[k] = true;
+                                     matrixDisp[i, j].GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);*/
+                                    if (turn % 2 == 0)
+                                    {
+                                        // Anula los clicks invalidos.
+                                        if (posX != 0 && posX != 5 && posY != 0 && posY != 5 && NoDisponibleArray[k] == false)
+                                        {
+                                            if(turn == 0)
+                                            {
+                                                Instantiate(prefabVirus[0], new Vector3(posX + 0.5f, -posY + 0.97f, -0.6f), prefabVirus[0].transform.rotation);
+                                            }
+                                            NoDisponibleArray[k] = true;
+                                            matrixDisp[i, j].GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);
+                                            cartelJuegaHumano.SetActive(true);
+                                            cartelJuegaVirus.SetActive(false);
+                                            tilesJugador1[cantTilesJ1] = matrixDisp[i, j];
+                                            cantTilesJ1++;
+                                            turn++;
+                                        }
 
-                                    tilesJugador1[cantTilesJ1] = matrixDisp[i, j];
-                                    cantTilesJ1++;
+
+                                    }
+                                    else
+                                    {
+                                        if (turn == 1)
+                                            Instantiate(prefabSalud[0], new Vector3(posX -0.4f, -posY -0.4f, -0.6f), prefabSalud[0].transform.rotation);
+                                        {
+                                            NoDisponibleArray[k] = true;
+                                            matrixDisp[i, j].GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.red);
+                                            cartelJuegaHumano.SetActive(false);
+                                            cartelJuegaVirus.SetActive(true);
+                                            tilesJugador2[cantTilesJ2] = matrixDisp[i, j];
+                                            cantTilesJ2++;
+                                            turn++;
+                                        }
+
+
+
+                                    }
                                 }
                                 else
                                 {
-                                    tilesJugador2[cantTilesJ2] = matrixDisp[i, j];
-                                    cantTilesJ2++;
-
+                                    k += 1;
                                 }
                             }
-                            else
-                            {
-                                k += 1;
-                            }
                         }
-                    }
 
-                    if (allUsed == false)
-                    {
-                        for (int i = 0; i < NoDisponibleArray.Length; i++)
+                        if (allUsed == false)
                         {
-                            allUsed = true;
-
-                            if (NoDisponibleArray[i] == true)
+                            for (int i = 0; i < NoDisponibleArray.Length; i++)
                             {
-                                allUsed = false;
+                                allUsed = true;
+
+                                if (NoDisponibleArray[i] == true)
+                                {
+                                    allUsed = false;
+                                }
                             }
                         }
+                        else
+                        {
+                            print("lleno");
+                        }
                     }
+
+
                 }
-                turn++;
 
             }
-            else
-            {
-                print("lleno");
-            }
+
         }
+        else
+        {
+            if (Physics.Raycast(ray, out hit, 100f, layerMask))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (hit.transform.name == "TILE(Clone)")
+                    {
+                        int posX = hit.transform.gameObject.GetComponent<tileData>().posX;
+                        int posY = hit.transform.gameObject.GetComponent<tileData>().posY;
 
-            
-        
+
+
+                    }
+                    
+                }
+            }
+
+
+        }
     }
-
-    
 }
