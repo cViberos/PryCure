5392a017da25e8368bc982778bdcd55fa7b1d732
Gonzalo Ezequiel Bordon <bordongonzaloezequiel@gmail.com>
2020-02-02 02:08:24 -0300

    nuevo hospital

---
+++ Assets/Imagenes/tilemax.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tilemax" tilewidth="100" tileheight="100" tilecount="15" columns="3">
+ <image source="tileset (2).png" width="300" height="500"/>
+</tileset>
